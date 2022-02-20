c6a32206dc2949aad1402665b4e30ebfa14f438c
jisooyu <jisooyu@hotmail.com>
2022-02-20 16:41:33 +0900

    seperate the fetchData in the routes

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const app = express();
-const fetchData = require('./fetchData/fetchData');
 
 app.use(express.json({ extened: false }));
 
-// send json for test
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to my world' });
-});
-
-// fetch data
-app.get('/fetch', async (req, res) => {
-  try {
-    const response = await fetchData();
-    console.log(response.data.response.body.items);
-    res.send(response.data.response.body.items);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(error);
-  }
-});
+// route handling for camp
+app.use('/api/camp', require('./routes/camp'));
 
 // PORT
 const PORT = process.env.PORT || 5000;
